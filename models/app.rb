ae19a3ee1af3448e252903ea25490a2295301cf5
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-07-17 13:31:54 -0400

    Done.

---
+++ models/app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+class App < Sinatra::Base 
+
+	get '/name' do
+  	"My name is __"
+	end
+
+	get '/hometown' do
+  	"My hometown is __"
+	end
+
+	get '/favorite-song' do
+  	"My favorite song is __"
+	end
+
+end 
